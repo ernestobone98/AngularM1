aa1b52cd8e4bd8be7ee79245c84c29074a2eb46d
ernestobone98 <ernesto.bone@outlook.com>
2022-10-04 18:54:56 +0200

    fc

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular';
+
 import { AppComponent } from './app.component';
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { RenduDirective } from './shared/rendu.directive';
