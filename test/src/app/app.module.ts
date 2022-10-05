30edcbad3aeccd3047f1492b676292ba201dc6a2
ernestobone98 <ernesto.bone@outlook.com>
2022-10-05 09:56:54 +0200

    modify material

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatButtonModule } from '@angular';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { RenduDirective } from './shared/rendu.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,9 @@ import { RenduDirective } from './shared/rendu.directive';
     RenduDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, MatButtonModule, MatIconModule, MatDividerModule,
+    MatInputModule, MatFormFieldModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
